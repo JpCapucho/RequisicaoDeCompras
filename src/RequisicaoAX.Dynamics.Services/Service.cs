20cf1df8291977b435c3ab77459e1598052b2e83
fabiano <fabianoconrado@gmail.com>
2017-12-09 00:24:52 -0200

    Incluindo serviços do Dynamics AX

---
+++ src/RequisicaoAX.Dynamics.Services/Service.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequisicaoAX.Dynamics.Services
+{
+    public class Service
+    {
+        public void fazerAlgo()
+        {
+            var client = new Purch.PurchRFQSitesServiceClient();
+            var result = client.read(GetContext(), new Purch.EntityKey[] {
+                new Purch.EntityKey {
+                    KeyData = new Purch.KeyField[] { new Purch.KeyField  {  Field = "", Value = ""} }
+                }
+            });
+
+
+            var proxy = new Purch.PurchReqImportServiceClient();
+            proxy.create(GetContext(), new Purch.AxdPurchReqImport
+            {
+                PurchReqTable = new Purch.AxdEntity_PurchReqTable[] {
+                    new Purch.AxdEntity_PurchReqTable {
+                        
+                    }
+                }
+            });
+        }
+
+
+        /// <summary>
+        /// Retorna o contexto atual
+        /// </summary>
+        /// <returns>object <see cref="Purch.CallContext"/></returns>
+        public Purch.CallContext GetContext()
+        {
+            return new Purch.CallContext { Company = "log", Language = "pt-br" };
+        }
+
+    }
+}
