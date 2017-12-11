3d34fbb13ffe7a00ade06853c23f8a85c346693a
João <jp.capucho@hotmail.com>
2017-12-11 16:47:59 -0200

    Incluido 3 classes DAO no projeto ERP

---
+++ src/RequisicaoAX.Dynamics/DAO/EcoResCategoryDAO.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using RequisicaoAX.Dynamics.Model;
+
+namespace RequisicaoAX.Dynamics.DAO
+{
+    class EcoResCategoryDAO
+    {
+        private SqlConnection Conn { get; set; }
+
+        /// <summary>
+        /// Contrutor com a string de conexao
+        /// </summary>
+        /// <param name="_conn"></param>
+        public EcoResCategoryDAO(SqlConnection _conn)
+        {
+            this.Conn = _conn;
+        }
+
+        /// <summary>
+        /// Construtor com a string de conexao
+        /// </summary>
+        /// <param name="_connString"></param>
+        public EcoResCategoryDAO(string _connString)
+        {
+            this.Conn = new SqlConnection(_connString);
+        }
+
+        /// <summary>
+        /// Select pelo RECID que retorna um objeto Produto
+        /// </summary>
+        /// <param name="_id"></param>
+        /// <returns></returns>
+        public EcoResCategory GetById(long _id)
+        {
+            var query = @"select * from [ECORESCATEGORY] where RECID = @Entrada ";
+
+            using (Conn)
+            {
+                try
+                {
+                    var resultado = Conn.Query<EcoResCategory>(query, new { Entrada = _id }).FirstOrDefault();
+                    return resultado;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Select All que retornar todos os objetos da tabela
+        /// </summary>
+        /// <returns></returns>
+        public List<EcoResCategory> GetAll()
+        {
+            var list = new List<EcoResCategory>();
+
+            using (Conn)
+            {
+                try
+                {
+                    var result = Conn.Query<EcoResCategory>("select * from [ECORESCATEGORY]");
+
+                    foreach (EcoResCategory categoria in result) {
+                        list.Add(categoria);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return list;
+            }
+        }
+    }
+}
---
+++ src/RequisicaoAX.Dynamics/DAO/PartitionDAO.cs
@@ -0,0 +1,86 @@
+﻿using RequisicaoAX.Dynamics.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace RequisicaoAX.Dynamics.DAO
+{
+    class PartitionDAO
+    {
+        private SqlConnection Conn { get; set; }
+
+
+        /// <summary>
+        /// Construtor com a string de conexao
+        /// </summary>
+        /// <param name="_conn"></param>
+        public PartitionDAO (SqlConnection _conn)
+        {
+            this.Conn = _conn;
+        }
+
+        /// <summary>
+        /// Construtor com a string de conexao
+        /// </summary>
+        /// <param name="_connString"></param>
+        public PartitionDAO (string _connString)
+        {
+            this.Conn = new SqlConnection(_connString);
+        }
+
+        /// <summary>
+        /// Select pelo RECID que retorna um objeto FiscalDocumentBr
+        /// </summary>
+        /// <param name="_id"></param>
+        /// <returns></returns>
+        public Partition GetById (long _id)
+        {
+            var query = @"select * from [PARTITIONS] where RECID = @Entrada";
+
+            using (Conn)
+            {
+                try
+                {
+                    var resultado = Conn.Query<Partition>(query, new { Entrada = _id }).FirstOrDefault();
+                    return resultado;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Select All que retorna todos os objetos da tabela
+        /// </summary>
+        /// <returns></returns>
+        public  List<FiscalEstablishment_BR> GetAll()
+        {
+            var list = new List<FiscalEstablishment_BR>();
+
+            using (Conn)
+            {
+                try
+                {
+                    var result = Conn.Query<FiscalEstablishment_BR>("select * from [PARTITIONS]");
+
+                    foreach (FiscalEstablishment_BR fiscalDocumentBr in result)
+                    {
+                        list.Add(fiscalDocumentBr);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                return list;
+            }
+        }
+    }
+}
