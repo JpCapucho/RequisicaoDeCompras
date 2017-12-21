8a2344768c363a457d60ce0b9a028f5f7adb0a0f
fabiano <fabianoconrado@gmail.com>
2017-12-21 20:33:37 -0200

    Incluindo classes basicas de dao

---
+++ src/RequisicaoAX.Business/EmpresaBusiness.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RequisicaoAX.Business
+{
+    public class EmpresaBusiness
+    {
+
+    }
+
+}
--- src/RequisicaoAX.Dynamics/Service.cs
+++ src/RequisicaoAX.Dynamics/Service.cs
@@ -23,113 +23,113 @@ namespace RequisicaoAX.Dynamics.Services
             //    }
             //});
 
-            var result = proxy.find(GetContext(), new Purch.QueryCriteria()
-            {
-                CriteriaElement = new Purch.CriteriaElement[]
-                {
-                    new Purch.CriteriaElement()
-                    {
-                        DataSourceName = "PurchReqTable",
-                        FieldName = "PurchReqId",
-                        Operator = Purch.Operator.Equal,
-                        Value1 = "025901",
-                    }
-                }
-            });
+            //var result = proxy.find(GetContext(), new Purch.QueryCriteria()
+            //{
+            //    CriteriaElement = new Purch.CriteriaElement[]
+            //    {
+            //        new Purch.CriteriaElement()
+            //        {
+            //            DataSourceName = "PurchReqTable",
+            //            FieldName = "PurchReqId",
+            //            Operator = Purch.Operator.Equal,
+            //            Value1 = "025901",
+            //        }
+            //    }
+            //});
 
 
 
-            var purchKey = proxy.create(GetContext(), new Purch.AxdPurchReqImport
-            {
-                PurchReqTable = new Purch.AxdEntity_PurchReqTable[] {
-                    new Purch.AxdEntity_PurchReqTable {
-                        PurchReqName = "FC AIF TESTE",
-                        RequiredDateSpecified = true,
-                        RequiredDate = DateTime.Now,
-                        TransDateSpecified = true,
-                        TransDate = DateTime.Now,
-                        //Originator = "5637190326",
+            //var purchKey = proxy.create(GetContext(), new Purch.AxdPurchReqImport
+            //{
+            //    PurchReqTable = new Purch.AxdEntity_PurchReqTable[] {
+            //        new Purch.AxdEntity_PurchReqTable {
+            //            PurchReqName = "FC AIF TESTE",
+            //            RequiredDateSpecified = true,
+            //            RequiredDate = DateTime.Now,
+            //            TransDateSpecified = true,
+            //            TransDate = DateTime.Now,
+            //            //Originator = "5637190326",
                         
-                        HeaderBusJustDetails = new Purch.AxdEntity_HeaderBusJustDetails[]
-                        {
-                            new Purch.AxdEntity_HeaderBusJustDetails
-                            {
-                                BusinessJustification = "COMPRA PLANEJADA",                                
-                                //action = Purch.AxdEnum_AxdEntityAction.create,
-                            }
-                        },
+            //            HeaderBusJustDetails = new Purch.AxdEntity_HeaderBusJustDetails[]
+            //            {
+            //                new Purch.AxdEntity_HeaderBusJustDetails
+            //                {
+            //                    BusinessJustification = "COMPRA PLANEJADA",                                
+            //                    //action = Purch.AxdEnum_AxdEntityAction.create,
+            //                }
+            //            },
 
-                        BusinessJustificationDescription = "Detalhes adicionais para justificativa do negócio",
+            //            BusinessJustificationDescription = "Detalhes adicionais para justificativa do negócio",
 
-                        ExternalSourceName = "DCI REQWEB",
-                        ExternalSourceID = "0001",
+            //            ExternalSourceName = "DCI REQWEB",
+            //            ExternalSourceID = "0001",
                         
-                        //se Projeto
-                        ProjId = "",
+            //            //se Projeto
+            //            ProjId = "",
 
-                        //status
-                        RequisitionStatusSpecified = true,
-                        RequisitionStatus = Purch.AxdEnum_PurchReqRequisitionStatus.InReview,
+            //            //status
+            //            RequisitionStatusSpecified = true,
+            //            RequisitionStatus = Purch.AxdEnum_PurchReqRequisitionStatus.InReview,
 
-                        //linhas da requisicao
-                        PurchReqLine = new Purch.AxdEntity_PurchReqLine[]
-                        {
-                            new Purch.AxdEntity_PurchReqLine
-                            {
+            //            //linhas da requisicao
+            //            PurchReqLine = new Purch.AxdEntity_PurchReqLine[]
+            //            {
+            //                new Purch.AxdEntity_PurchReqLine
+            //                {
 
-                                PurchQty = 10,
-                                PurchQtySpecified = true,
-                                BuyingLegalEntityDataArea = "LOG",
+            //                    PurchQty = 10,
+            //                    PurchQtySpecified = true,
+            //                    BuyingLegalEntityDataArea = "LOG",
 
-                                InventDim = new Purch.AxdEntity_InventDim[]
-                                {
-                                    new Purch.AxdEntity_InventDim()
-                                    {
-                                        InventSiteId = "LOG_SJC",
-                                        InventColorId = "AZUL",
-                                        ConfigId = "PINCEL",
-                                        InventSizeId = "",
-                                        InventLocationId = "",
-                                    }
-                                },
-                                ItemId = "400011000",
-                                //Name = "CANETA",
-                                //DeliveryName = "CANETA DELIVERY",
+            //                    InventDim = new Purch.AxdEntity_InventDim[]
+            //                    {
+            //                        new Purch.AxdEntity_InventDim()
+            //                        {
+            //                            InventSiteId = "LOG_SJC",
+            //                            InventColorId = "AZUL",
+            //                            ConfigId = "PINCEL",
+            //                            InventSizeId = "",
+            //                            InventLocationId = "",
+            //                        }
+            //                    },
+            //                    ItemId = "400011000",
+            //                    //Name = "CANETA",
+            //                    //DeliveryName = "CANETA DELIVERY",
                                 
-                                PriceUnitSpecified = true,
-                                PriceUnit = 1,
-                                CurrencyCode = "BRL",
-                                ExternalItemId = "",
-                                RequisitionStatusSpecified = true,
-                                RequisitionStatus = Purch.AxdEnum_PurchReqRequisitionStatus.Draft,
-                                //InventLocationId = "",
+            //                    PriceUnitSpecified = true,
+            //                    PriceUnit = 1,
+            //                    CurrencyCode = "BRL",
+            //                    ExternalItemId = "",
+            //                    RequisitionStatusSpecified = true,
+            //                    RequisitionStatus = Purch.AxdEnum_PurchReqRequisitionStatus.Draft,
+            //                    //InventLocationId = "",
 
-                                Requisitioner = "000365", //JP
+            //                    Requisitioner = "000365", //JP
 
-                                RequiredDateSpecified = true,
-                                RequiredDate = DateTime.Now,
-                                TransDateSpecified = true,
-                                TransDate = DateTime.Now,
+            //                    RequiredDateSpecified = true,
+            //                    RequiredDate = DateTime.Now,
+            //                    TransDateSpecified = true,
+            //                    TransDate = DateTime.Now,
 
-                            }
-                        }
-                    }
+            //                }
+            //            }
+            //        }
 
 
-                },
+            //    },
 
-                //DocPurpose = Purch.AxdEnum_XMLDocPurpose.Original,
+            //    //DocPurpose = Purch.AxdEnum_XMLDocPurpose.Original,
 
-                //ClearNilFieldsOnUpdate = Purch.AxdEnum_boolean.@true,
+            //    //ClearNilFieldsOnUpdate = Purch.AxdEnum_boolean.@true,
 
-                //ClearNilFieldsOnUpdateSpecified = true,
+            //    //ClearNilFieldsOnUpdateSpecified = true,
 
-                //DocPurposeSpecified = true,
+            //    //DocPurposeSpecified = true,
 
-                //SenderId = ""
-            });
+            //    //SenderId = ""
+            //});
 
-            var x = purchKey.ToString();
+            //var x = purchKey.ToString();
         }
 
 
